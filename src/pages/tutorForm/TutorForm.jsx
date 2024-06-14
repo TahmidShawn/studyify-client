a93c220147b98c495f9d166e559ff42767745e2b
TahmidShawn7 <md.tahmid022@gmail.com>
2024-06-14 23:44:22 +0600

    feat: setup tutor data to post in database

--- src/pages/tutorForm/TutorForm.jsx
+++ src/pages/tutorForm/TutorForm.jsx
@@ -1,5 +1,7 @@
 import { useForm } from "react-hook-form";
 import useAxiosPublic from "../../hooks/useAxiosPublic";
+import tutorImg from "./../../assets/tutor.jpg";
+import toast from "react-hot-toast";
 
 const TutorForm = () => {
   const { register, handleSubmit, reset } = useForm();
@@ -8,26 +10,39 @@ const TutorForm = () => {
   const img_hosting_api = `https://api.imgbb.com/1/upload?key=${image_hosting_key}`;
 
   const onSubmit = async (data) => {
-    console.log(data);
-
     const file = data.tutorImage[0];
     delete data.tutorImage;
     const formData = new FormData();
     formData.append("key", image_hosting_key);
     formData.append("image", file);
     // send img to imgBB
-    const res = await axiosPublic?.post(img_hosting_api, formData, {
-      headers: {
-        "Content-Type": "multipart/form-data",
-      },
-    });
-    const imageUrl = res.data.data.url;
-    console.log(imageUrl);
+    try {
+      const res = await axiosPublic?.post(img_hosting_api, formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+      const imageUrl = res.data.data.url;
+      const role = "teacher";
+      //   added imageUrl and role
+      data.tutorImage = imageUrl;
+      data.role = role;
 
-    // Add imageUrl and role to data
-    data.imageUrl = imageUrl;
-
-    console.log(data);
+      console.log(data);
+      try {
+        axiosPublic.post("api/tutors", data).then((res) => {
+          console.log(res);
+          if (res.data.tutorEmail) {
+            toast.success("Congratulations");
+            reset();
+          }
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    } catch (error) {
+      toast.error("Something went wrong.Please try with a different photo");
+    }
   };
 
   return (
@@ -35,21 +50,17 @@ const TutorForm = () => {
       <h2 className="text-4xl text-center font-semibold mt-4">
         Teach on Studyify
       </h2>
-     
-      <div className="p-4 mt-4 grid grid-cols-1 md:grid-cols-3 gap-12">
-        <div className="hidden md:inline-block h-screen min-h-full col-span-1">
-          <img
-            src="https://readymadeui.com/image-3.webp"
-            className="w-full h-full "
-            alt="image"
-          />
+
+      <div className="p-4 mt-4 grid grid-cols-2 md:grid-cols-3 gap-12">
+        <div className="col-span-2 md:col-span-1 order-2 md:order-1">
+          <img src={tutorImg} className="w-full" alt="image" />
         </div>
-        <div className=" lg:mx-auto max-w-screen-xl  border-[1px] border-gray-300 p-8 col-span-2 w-full">
+        <div className=" lg:mx-auto max-w-screen-xl  border-[1px] border-gray-300 p-8 col-span-2 w-full order-1 md:order-2">
           <form
             onSubmit={handleSubmit(onSubmit)}
             className="px-8 grid sm:grid-cols-2 gap-6"
           >
-            <div className="col-span-full">
+            <div className="col-span-full lg:col-span-1">
               <label className="font-semibold text-sm">Your Name</label>
               <input
                 {...register("tutorName", { required: true })}
@@ -58,7 +69,7 @@ const TutorForm = () => {
                 className="w-full rounded py-2.5 px-4 mt-2 border-[1px] text-sm outline-[#007bff]"
               />
             </div>
-            <div className="col-span-full">
+            <div className="col-span-full lg:col-span-1">
               <label className="font-semibold text-sm">Email</label>
               <input
                 {...register("tutorEmail", { required: true })}
